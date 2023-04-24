75b1de6e76093552b74b52982b06370749801604
unknown <2199630705@qq.com>
2023-04-24 14:40:49 +0800

    feat(custom): 配置路由和404

--- src/App.vue
+++ src/App.vue
@@ -37,6 +37,9 @@ import HelloWorld from './components/HelloWorld.vue'
     <el-icon size="32" class="text-sky-500">
       <Edit />
     </el-icon>
+    <template>
+      <router-view />
+    </template>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Index from '@/views/index.vue'
+import Login from '@/views/login/index.vue'
+import NotFound from '@/views/404.vue'
+
+const routes =[
+  {
+    path: '/',
+    name: 'index',
+    component: Index
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },{
+    path: '/:pathMatch(.*)*',
+    name: 'home',
+    component: NotFound
+  },
+] 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes,
+  history: createWebHistory()
 })
 
 export default router
