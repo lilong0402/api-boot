547241c86e518d049d9925f0d93c37bbfc285962
unknown <2199630705@qq.com>
2023-04-24 14:59:42 +0800

    feat(custom): 引入axios和后端接口交互

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,19 @@
+<script setup>
+import { login } from '@/api/auth'
+const form = reactive({
+  username: 'admin',
+  password: '123456'
+})
+
+const handleLogin = () => {
+  login(form).then((res) => {
+    console.log(res.data.data)
+  })
+}
+</script>
+
 <template>
-  <div></div>
+  <div>
+    <el-button type="primary" size="default" @click="handleLogin">登录</el-button>
+  </div>
 </template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
