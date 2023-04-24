cba7acd3ebc5df66ac167e519e90f9d2d6d8f71c
unknown <2199630705@qq.com>
2023-04-24 16:41:35 +0800

    feat(custom): 封装通用抽屉组件

--- src/layout/admin.vue
+++ src/layout/admin.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { useAdminStore } from '../stores'
+const store = useAdminStore()
+const { sideWidth } = storeToRefs(store)
+</script>
+
 <template>
   <el-container>
     <el-header>
@@ -5,14 +11,49 @@
     </el-header>
 
     <el-container>
-      <el-aside>
+      <el-aside :style="{ width: sideWidth }">
         <side-menu />
       </el-aside>
 
       <el-main>
         <bread-crumbs />
-        <router-view></router-view>
+        <router-view v-slot="{ Component }">
+          <transition name="fade">
+            <keep-alive :max="10">
+              <component :is="Component"></component>
+            </keep-alive>
+          </transition>
+        </router-view>
       </el-main>
     </el-container>
   </el-container>
 </template>
+
+<style scoped>
+.el-main {
+  @apply w-full h-screen text-left;
+  padding: 0;
+}
+.el-aside {
+  transition: all 0.2s;
+}
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s;
+}
+.fade-enter-active {
+  transition-delay: 0.3s;
+}
+</style>
